6235e1119563f7f10aae2ce4592b4b770a2d38b7
shota <shota_takehara@kronos.jp>
2023-10-12 15:12:57 +0900

    line rogin

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,61 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const request = require('request');
+const querystring = require('querystring');
+
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, 'public'));
+app.disable('etag');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello World!' });
+});
+
+app.get('/login', (req, res) => {
+  const query = querystring.stringify({
+    response_type: 'code',
+    client_id: process.env.LINECORP_PLATFORM_CHANNEL_CHANNELID,
+    redirect_uri: 'https://nodejsreactshowtake.herokuapp.com/callback',
+    state: 'hoge', // TODO: must generate random string
+    scope: 'profile',
+  });
+  res.redirect(301, 'https://access.line.me/oauth2/v2.1/authorize?' + query);
+});
+
+app.get('/callback', (req, res) => {
+  request.post({
+    url: `https://api.line.me/oauth2/v2.1/token`,
+    form: {
+      grant_type: "authorization_code",
+      code: req.query.code,
+      redirect_uri: 'https://nodejsreactshowtake.herokuapp.com/callback',
+      client_id: process.env.LINECORP_PLATFORM_CHANNEL_CHANNELID,
+      client_secret: process.env.LINECORP_PLATFORM_CHANNEL_CHANNELSECRET,
+    }
+  }, (error, response, body) => {
+    if (response.statusCode != 200) {
+      res.send(error);
+      return;
+    }
+    request.get({
+      url: 'https://api.line.me/v2/profile',
+      headers: {
+        'Authorization': 'Bearer ' + JSON.parse(body).access_token
+      }
+    }, (error, response, body) => {
+      if (response.statusCode != 200) {
+        res.send(error);
+        return;
+      }
+      res.send(body);
+    });
+  });
 });
 
 app.get('*', (req, res) => {
@@ -14,5 +63,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`listening on *:${port}`);
+  console.log(`Listening on *:${port}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import React from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
-  useEffect(() => {
+  const [message, setMessage] = React.useState('');
+
+  React.useEffect(() => {
     fetch('/api')
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
+
   return (
     <div className="App">
+      <h1>共助アプリ認証</h1>
+      <button
+        type="button"
+        className="btn btn-lg btn-default"
+        onClick={() => {
+          // ボタンがクリックされたときに指定されたURLに遷移
+          window.location.href = '/login';
+        }}
+      >
+        LINEログイン
+      </button>
       <h1>フロントエンド</h1>
       <p>{message}</p>
     </div>
